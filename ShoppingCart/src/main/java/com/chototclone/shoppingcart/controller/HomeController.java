9d9bca0e84a75353621068adfd0875f9773f6699
VoHoangKiet <vohoangkiet.dev2510@gmail.com>
2024-08-26 11:07:48 +0700

    feat: base layout - navabar

---
+++ ShoppingCart/src/main/java/com/chototclone/shoppingcart/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.chototclone.shoppingcart.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
